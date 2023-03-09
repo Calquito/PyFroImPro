e29a088c3f6f51c8e5fd1c22f325d96228f392ac
Calquito <39011234+Calquito@users.noreply.github.com>
2023-03-08 22:08:03 -0600

    Implement ReadImageDatabase

---
+++ callFunctions.py
@@ -0,0 +1,8 @@
+from ReadImageDataBase import ReadImageDataBase
+
+#Llamado de prueba a la función ReadImageDataBase
+A_ReadImageDataBase,m_ReadImageDataBase,n_ReadImageDataBase = ReadImageDataBase('image database')
+
+
+
+
