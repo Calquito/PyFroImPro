58c12a53728139b139a1aabb66d1737837354566
Calquito <39011234+Calquito@users.noreply.github.com>
2023-03-15 21:33:52 -0600

    add Matrix2Video

--- funciones_python/callFunctions.py
+++ funciones_python/callFunctions.py
@@ -1,19 +1,30 @@
 from ReadImageDataBase import ReadImageDataBase
 from Matrix2Image import Matrix2Image
 from double2uint8 import double2uint8
+from Video2Matrix import Video2Matrix
+from Matrix2Video import Matrix2Video
+import os
+import numpy as np
 
 #Llamado de prueba a la función ReadImageDataBase
-A_ReadImageDataBase,m_ReadImageDataBase,n_ReadImageDataBase = ReadImageDataBase('image database')
+#A_ReadImageDataBase,m_ReadImageDataBase,n_ReadImageDataBase = ReadImageDataBase('ImageDatabase')
 
 #Llamado de prueba a la función double2uint8
-A_double2uint8 = double2uint8(A_ReadImageDataBase)
+#A_double2uint8 = double2uint8(A_ReadImageDataBase)
 
 #Llamado de prueba a la función Matrix2Image
-Matrix2Image(A_ReadImageDataBase,m_ReadImageDataBase,n_ReadImageDataBase,'Matrix2ImageResults','Matrix2ImageResult_')
+#Matrix2Image(A_ReadImageDataBase,m_ReadImageDataBase,n_ReadImageDataBase,'Matrix2ImageResults','Matrix2ImageResult_')
 
+#Llamado de prueba a la función video2Matrix
+#XO_video2Matrix, m1, n1 = Video2Matrix('Video2MatrixExample/video.mp4',0.1,100)
 
+#Llamado de prueba a la función video2Matrix con scale y numFrames
+#XO_video2Matrix, m1, n1 = Video2Matrix('Video2MatrixExample/video.mp4',0.1,100)
 
+#np.set_printoptions(precision=2, threshold=np.inf)
+#print(XO_video2Matrix)
 
-
+#Llamado de prueba a la función Matrix2Video
+#Matrix2Video(XO_video2Matrix,m1,n1,'Matrix2VideoResults\\Matrix2VideoResult.mp4')
 
 
